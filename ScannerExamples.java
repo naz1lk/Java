2eed9ea137241a3d278887e9f9abd14f8f9ae041
naz1lk <126571335+naz1lk@users.noreply.github.com>
2023-03-13 16:57:32 +0100

    homework
    Java

---
+++ ScannerExamples.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class ScannerExamples {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.print("Enter your age: ");
+//        int age = sc.nextInt();
+
+//        sc.nextLine();
+
+//        System.out.print("Enter your name: ");
+//        String name = sc.nextLine();
+
+//        System.out.print("Enter your surname: ");
+//        String surname = sc.next();
+
+//        System.out.println("Your full name is " + name + " " + surname);
+
+
+        //int -> .nexIt()
+//        System.out.println("Enter pls which year today: ");
+//        int year = sc.nextInt();
+//        System.out.println("You selected your is year " + year);
+
+        //double -> .nexDouble()
+
+//        System.out.println("Enter pls radius: ");
+//        double radius = sc.nextDouble();
+//        double result = Math.pow(radius, 2) * Math.PI;
+//        System.out.println("radius = " + result);
+
+        //int example
+//        System.out.print("Enter integer value: ");
+//        int value = sc.nextInt();
+//        System.out.println("Decimal value: " + value);
+//        System.out.println("Binary value: " + Integer.toBinaryString(value));
+//        System.out.println("Octal value " + Integer.toOctalString(value));
+//        System.out.println("Hex value: " + Integer.toHexString(value));
+
+
+        System.out.println("Enter the number: 123 ");
+        int m = sc.nextInt();
+
+
+
+        System.out.println();
+
+//        System.out.println(123 / 3);    // 41
+//        System.out.println(123 % 3);      // 0
+
+//        System.out.println(41 / 3);    // 13
+//        System.out.println(41 % 3);      // 2
+
+//        System.out.println(13 / 3);    // 4
+//        System.out.println(13 % 3);      // 1
+
+//        System.out.println(4 / 3);    // 1
+//        System.out.println(4 % 3);      // 1
+
+//        System.out.println(1 / 3);    // 0
+//        System.out.println(1 % 3);      // 1
+
+        System.out.println("10 -> 3 = 11120 ");
+
+
+        sc.close();
+    }
+}
