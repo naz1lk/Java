560babe64d1b57cc604679f7896aa96383d25ff3
naz1lk <126571335+naz1lk@users.noreply.github.com>
2023-03-15 16:50:00 +0100

    homework
    java

---
+++ Homework.java
@@ -0,0 +1,14 @@
+public class Homework {
+    public static void main(String[] args) {
+
+        String a = "Hello, Java! It's a Java Basic course";
+        System.out.println(a.length());
+        System.out.println(a.charAt(11));
+        System.out.println(a.substring(12,25));
+        System.out.println(a.substring(19));
+
+
+
+
+    }
+}
