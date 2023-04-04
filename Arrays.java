30c0971838bcb476540ce9c155c0304cf3a55a25
naz1lk <126571335+naz1lk@users.noreply.github.com>
2023-04-04 16:52:04 +0200

    homework
    Java

---
+++ Arrays.java
@@ -0,0 +1,18 @@
+package homeworks;
+
+import java.util.Random;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int b = 0;
+        int[] Mas = new int[15];
+        for (int i = 0; i < Mas.length; i++) {
+            Random rnd = new Random();
+            Mas[i] = rnd.nextInt(10);
+            System.out.print(Mas[i] + " ");
+            if (Mas[i] > 0 & Mas[i] % 2 == 0) b++;
+        }
+        System.out.println(" ");
+        System.out.println("Всего в массиве " + b + " четных");
+    }
+}
